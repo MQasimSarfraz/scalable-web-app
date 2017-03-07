aee67dab072e8099e3565bcd59f504368a5f8cd5
MQasimSarfraz <syed.qasim.sarfraz@gmail.com>
2017-03-08 03:33:29 +0500

    Client to use internal IP

--- kubernetes/scripts/configure-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# The script will be used to update the SERVER IP in the client file
-
-SERVER=$(kubectl config view | grep server| head -1 | tr -d ' '| cut -f3 -d':' | sed 's/\/\///g')
-sed -i "s/<NODE_IP>/$SERVER/g" ../deployments/client.yaml
